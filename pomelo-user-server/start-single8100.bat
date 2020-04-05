7674e8e6bd1690d4c85cd5f0f0b218ffc1a2bb94
fengchao <chao.feng9@pactera.com>
2020-04-05 21:46:53 +0800

    feat: env

--- pomelo-user-server/start-single8100.bat
+++ pomelo-user-server/start-single8100.bat
@@ -1,2 +1,2 @@
 cd ./target
-java -jar pomelo-user-server-0.0.1.jar --server.port=8100 --eureka.client.serviceUrl.defaultZone=http://pomelo:pomelo@localhost:8760/eureka/
+java -jar pomelo-user-server-0.0.1.jar --spring.profiles.active=test
